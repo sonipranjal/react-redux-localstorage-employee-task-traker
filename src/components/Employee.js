9b8358d04baeee7f25483d5ca667e5a83849f35d
sonipranjal <pranjalsoni6256@gmail.com>
2021-09-08 00:21:48 +0530

    button change

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,28 +2,35 @@ import { Link } from 'react-router-dom';
 
 const Employee = ({ name, phoneNumber, email, slug }) => {
   return (
-    <tr>
-      <td className="px-5 py-5 text-sm bg-white border-gray-200">
-        <div className="flex items-center">
-          <p className="text-gray-900 capitalize whitespace-no-wrap">{name}</p>
-        </div>
-      </td>
-      <td className="px-5 py-5 text-sm bg-white border-gray-200">
-        <p className="text-gray-900 whitespace-no-wrap">{phoneNumber}</p>
-      </td>
-      <td className="px-5 py-5 text-sm bg-white border-gray-200">
-        <p className="text-gray-900 whitespace-no-wrap">{email}</p>
-      </td>
-      <td className="px-5 py-5 text-sm bg-white border-b border-gray-200">
-        <Link
-          to={`/employee/${slug}`}
-          key={slug}
-          className="px-4 py-2 text-white transition delay-75 bg-indigo-500 rounded-2xl hover:bg-indigo-900"
+    <>
+      <tr>
+        <td className="px-5 py-5 text-sm bg-white border-gray-200">
+          <div className="flex items-center">
+            <p className="text-gray-900 capitalize whitespace-no-wrap">
+              {name}
+            </p>
+          </div>
+        </td>
+        <td className="px-5 py-5 text-sm bg-white border-gray-200">
+          <p className="text-gray-900 whitespace-no-wrap">{phoneNumber}</p>
+        </td>
+        <td className="px-5 py-5 text-sm bg-white border-gray-200">
+          <p className="text-gray-900 whitespace-no-wrap">{email}</p>
+        </td>
+        <td
+          colSpan="4"
+          className="px-5 py-5 bg-white border-b border-gray-200 "
         >
-          View Details
-        </Link>
-      </td>
-    </tr>
+          <Link
+            to={`/employee/${slug}`}
+            key={slug}
+            className="px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-indigo-600 rounded-lg shadow-md hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2"
+          >
+            Details
+          </Link>
+        </td>
+      </tr>
+    </>
   );
 };
 
